b02d627c11d2b5171a2d867997f19286a2e3fbdb
Bijay Das <imbijaydas@gmail.com>
2020-03-08 12:38:06 +0530

    Added routes section

--- public/js/master.js
+++ public/js/master.js
@@ -1,3 +1,10 @@
 (function() {
     hljs.initHighlightingOnLoad();
+    
+    const _linksToStop = document.querySelectorAll('.lnk-stop');
+    for (const _linkToStop of _linksToStop) {
+        _linkToStop.addEventListener('click', function(event) {
+            event.preventDefault();
+        });
+    }
 })();
